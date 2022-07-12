3015cc20f236cdf148ca32243e266e14c06dfbcb
YuriyGl86 <yuriygl86@gmail.com>
2022-07-12 22:51:43 +0300

    осталось функции допилить

--- OOP.py
+++ OOP.py
@@ -9,34 +9,37 @@ class Student:
         self.grades = {}
 
     def rate_lecturer(self, lecturer, course, mark):
-        if isinstance(lecturer,
-                      Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
-            lecturer.marks[course] = lecturer.marks.get(course, []) + [mark]  # ---
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            lecturer.grades[course] = lecturer.grades.get(course, []) + [mark]  # ---
         else:
             return 'Ошибка'
 
-    def get_avrg_grade(self):
+    def __get_avrg_grade(self):
         all_grades = sum(self.grades.values(), [])
-        avrg_grade = sum(all_grades) / len(all_grades)
-        return avrg_grade
+        if len(all_grades) != 0:
+            avrg_grade = sum(all_grades) / len(all_grades)
+            return avrg_grade
+        else:
+            return 'нет оценок'
+
 
     def __str__(self):
         return f'Имя: {self.name}\nФамилия: {self.surname}\n' \
-               f'Средняя оценка за домашние задания: {self.get_avrg_grade()}\n' \
+               f'Средняя оценка за домашние задания: {self.__get_avrg_grade()}\n' \
                f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n' \
                f'Завершенные курсы: {", ".join(self.finished_courses)}'
 
     def __lt__(self, other):
         if isinstance(other, Student):
-            return self.get_avrg_grade() < other.get_avrg_grade()
+            return self.__get_avrg_grade() < other.__get_avrg_grade()
 
     def __eq__(self, other):
         if isinstance(other, Student):
-            return self.get_avrg_grade() == other.get_avrg_grade()
+            return self.__get_avrg_grade() == other.__get_avrg_grade()
 
     def __le__(self, other):
         if isinstance(other, Student):
-            return self.get_avrg_grade() <= other.get_avrg_grade()
+            return self.__get_avrg_grade() <= other.__get_avrg_grade()
 
 
 class Mentor:
@@ -47,10 +50,9 @@ class Mentor:
         self.courses_attached = []
 
     def rate_hw(self, student, course, grade):
-        if isinstance(student,
-                      Student) and course in self.courses_attached and course in student.courses_in_progress and isinstance(
-                self, Reviewer):
-            student.grades[course] = student.grades.get(course, []) + [grade]  # ---
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress \
+                and isinstance(self, Reviewer):
+            student.grades[course] = student.grades.get(course, []) + [grade]
         else:
             return 'Ошибка'
 
@@ -60,25 +62,28 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
-    def get_avrg_grade(self):
+    def __get_avrg_grade(self):
         all_grades = sum(self.grades.values(), [])
-        avrg_grade = sum(all_grades) / len(all_grades)
-        return avrg_grade
+        if len(all_grades) != 0:
+            avrg_grade = sum(all_grades) / len(all_grades)
+            return avrg_grade
+        else:
+            return 'нет оценок'
 
     def __str__(self):
-        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.get_avrg_grade()}'
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.__get_avrg_grade()}'
 
     def __lt__(self, other):
         if isinstance(other, Lecturer):
-            return self.get_avrg_grade() < other.get_avrg_grade()
+            return self.__get_avrg_grade() < other.__get_avrg_grade()
 
     def __eq__(self, other):
         if isinstance(other, Lecturer):
-            return self.get_avrg_grade() == other.get_avrg_grade()
+            return self.__get_avrg_grade() == other.__get_avrg_grade()
 
     def __le__(self, other):
         if isinstance(other, Lecturer):
-            return self.get_avrg_grade() <= other.get_avrg_grade()
+            return self.__get_avrg_grade() <= other.__get_avrg_grade()
 
 
 class Reviewer(Mentor):
@@ -87,33 +92,53 @@ class Reviewer(Mentor):
         return f'Имя: {self.name}\nФамилия: {self.surname}'
 
 
+# def avrg_by_cours(students, cours):
+
+
+
 best_student = Student('Ivan', 'Ivanov', 'male')
 best_student.courses_in_progress += ['Python']
 best_student.courses_in_progress += ['Git']
-cool_mentor = Reviewer('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-cool_mentor.courses_attached += ['Git']
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 20)
-cool_mentor.rate_hw(best_student, 'Git', 30)
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
+cool_reviewer.courses_attached += ['Git']
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 20)
+cool_reviewer.rate_hw(best_student, 'Git', 30)
 print(best_student)
 print()
 
 best_student2 = Student('Petr', 'Petrov', 'male')
 best_student2.courses_in_progress += ['Python']
 best_student2.courses_in_progress += ['Java', 'C++']
-cool_mentor2 = Reviewer('Some2', 'Buddy2')
-cool_mentor2.courses_attached += ['Python', 'Java', 'C++']
-cool_mentor2.rate_hw(best_student2, 'Python', 10)
-cool_mentor2.rate_hw(best_student2, 'Java', 40)
-cool_mentor2.rate_hw(best_student2, 'C++', 5)
+cool_reviewer2 = Reviewer('Some2', 'Buddy2')
+cool_reviewer2.courses_attached += ['Python', 'Java', 'C++']
+cool_reviewer2.rate_hw(best_student2, 'Python', 10)
+cool_reviewer2.rate_hw(best_student2, 'Java', 40)
+cool_reviewer2.rate_hw(best_student2, 'C++', 5)
 print(best_student2)
 print()
+print(cool_reviewer2)
+print()
 
-cool_mentor3 = Lecturer('Some3', 'Buddy3')
-cool_mentor3.courses_attached += ['Python', 'Java', 'C++']
-print(cool_mentor3.rate_hw(best_student2, 'Python', 10))  # ошибка, лектор не может давать оценок
+cool_Lecturer = Lecturer('Some3', 'Buddy3')
+cool_Lecturer.courses_attached += ['Python', 'Java', 'C++']
+print(cool_Lecturer.rate_hw(best_student2, 'Python', 10))  # ошибка, оценка не добавляется, лектор не может давать оценок
+best_student.rate_lecturer(cool_Lecturer, 'Python', 10)
+best_student2.rate_lecturer(cool_Lecturer, 'Java', 20)
+print(cool_Lecturer)
+print()
+cool_Lecturer2 = Lecturer('Some4', 'Buddy4')
+cool_Lecturer2.courses_attached += ['Python', 'Java', 'C++']
+best_student.rate_lecturer(cool_Lecturer2, 'Python', 3)
+best_student2.rate_lecturer(cool_Lecturer2, 'Java', 33)
+print(cool_Lecturer2)
+print()
 
 print(best_student == best_student2)
 print(best_student < best_student2)
 print(best_student >= best_student2)
+print()
+print(cool_Lecturer2 == cool_Lecturer)
+print(cool_Lecturer2 < cool_Lecturer)
+print(cool_Lecturer2 >= cool_Lecturer)
